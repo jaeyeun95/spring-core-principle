2d03eaf960bab1ca12c1ea69835d0a3113ae98fc
jaeyeun <jaeyeun95@gmail.com>
2022-01-10 22:16:33 +0900

    의존관계 자동 주입 롬복라이브러리 사용법, @RequiredArgsConstructor 를 사용하여 코드 간결화

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,42 +4,18 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+//  롬북에서 제공해주는 @RequiredArgsConstructor 어노테이션을 사용하면 생성자 코드를 자동으로 만들어 준다.
 public class OrderServiceImpl implements OrderService {
-
-//    private final MemberRepository memberRepository = new MemoryMemberRepository();
-
-//    아래와 같이 하면 인터페이스와 구현 클래스 둘다에 의존하게 되는 것이다. -> DIP 위반
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     // 생성자 주입
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    // setter 주입(수정자 주입)
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository){
-//        System.out.println("member");
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy){
-//        System.out.println("discount");
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("생성자 주입");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
