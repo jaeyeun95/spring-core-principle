b369c2def78a9e6a319bebc6f629348ebb102dab
jaeyeun <jaeyeun95@gmail.com>
2022-01-29 22:40:57 +0900

    스프링 DB 접근 기술 JPA 설정파일 추가 및 적용

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // id를 자동으로 증가시켜주는 것 oracle 에서 sequence
     private Long id;
+
     private String name;
 
     public Long getId() {
