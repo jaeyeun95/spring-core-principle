f30147600f73b1943dd8d4add5cbfcd0574e523e
jaeyeun <jaeyeun95@gmail.com>
2022-02-27 22:04:40 +0900

    타임리프에서 제공하는 기본 객체들

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,14 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,22 +16,23 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
+@Slf4j
 public class BasicController {
 
     @GetMapping("text-basic")
-    public String textBasic(Model model){
+    public String textBasic(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-basic";
     }
 
     @GetMapping("text-unescaped")
-    public String textUnescaped(Model model){
+    public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
     }
 
     @GetMapping("/variable")
-    public String variable(Model model){
+    public String variable(Model model) {
         User userA = new User("userA", 10);
         User userB = new User("userA", 10);
 
@@ -47,14 +51,30 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObject(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-object";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            log.info("### HelloBean ####");
+            System.out.println("ComponentScan");
+            return "Hello " + data;
+        }
+    }
+
+
     @Data
     static class User {
         private String username;
         private int age;
 
-        public User(String username, int age){
+        public User(String username, int age) {
             this.username = username;
-            this.age =age;
+            this.age = age;
         }
     }
 }
