fb5ed0fd24111e6fabb44c426fda87c42bf60ab9
jaeyeun95 <jaeyeun95@gmail.com>
2022-09-01 20:14:17 +0900

    로그추적기 V2 적용

---
+++ advanced/src/main/java/com/example/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,32 @@
+package com.example.advanced.app.v2;
+
+import com.example.advanced.trace.TraceId;
+import com.example.advanced.trace.TraceStatus;
+import com.example.advanced.trace.hellotrace.HelloTraceV1;
+import com.example.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId){
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId,"OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 한다. 예외를 던지지 않으면 catch에서 탈출이 불가능
+        }
+
+        orderRepository.save(status.getTraceId(), itemId);
+    }
+}
